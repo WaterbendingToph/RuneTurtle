b0048feafb4776cf2795511f05610c2eeede748d
WaterbendingToph <dakotago@lakotaarms45@gmail.com>
2022-12-02 16:52:46 -0700

    Add Basic Letter Structure Files

---
+++ CypherLibrary/HowToTrainYourDragon/x.py
@@ -0,0 +1,15 @@
+import turtle
+window = turtle.Screen()
+window.setup(width = 500, height=500)
+
+letterWidth = 200
+lineWidth = 20
+letterHeight = 250
+diagonal = 60
+
+turtle.mode("logo")
+turtle.penup()
+turtle.goto(-10, -125)
+turtle.pendown()
+
+turtle.done()
