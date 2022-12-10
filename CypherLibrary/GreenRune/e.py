107d2d81d3954729079c85746aac3854f6a44064
WaterbendingToph <dakotago@lakotaarms45@gmail.com>
2022-12-09 17:24:50 -0700

    setup symbol file structure for GreenRune

---
+++ CypherLibrary/GreenRune/e.py
@@ -0,0 +1,27 @@
+import turtle
+window = turtle.Screen()
+window.setup(width = 500, height=500)
+
+letterWidth = 200
+lineWidth = 20
+letterHeight = 250
+diagonal = 60
+
+turtle.mode("logo")
+turtle.penup()
+turtle.goto(-10, -125)
+turtle.pendown()
+
+#START
+turtle.begin_fill()
+
+turtle.end_fill()
+#END
+
+#SINGULAR START
+turtle.begin_fill()
+
+turtle.end_fill()
+#SINGULAR END
+
+turtle.done()
---
+++ CypherLibrary/GreenRune/openParen.py
@@ -0,0 +1,21 @@
+import turtle
+window = turtle.Screen()
+window.setup(width = 500, height=500)
+
+letterWidth = 200
+lineWidth = 20
+letterHeight = 250
+diagonal = 60
+
+turtle.mode("logo")
+turtle.penup()
+turtle.goto(-10, -125)
+turtle.pendown()
+
+#START
+turtle.begin_fill()
+
+turtle.end_fill()
+#END
+
+turtle.done()
