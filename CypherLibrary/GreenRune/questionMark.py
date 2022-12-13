d20f70ed199271f55cc6426c06bc01f186d6747f
WaterbendingToph <dakotago@lakotaarms45@gmail.com>
2022-12-13 10:05:39 -0700

    Add All Green Rune Symbol Files

--- CypherLibrary/GreenRune/questionMark.py
+++ CypherLibrary/GreenRune/questionMark.py
@@ -2,18 +2,37 @@ import turtle
 window = turtle.Screen()
 window.setup(width = 500, height=500)
 
-letterWidth = 200
 letterHeight = 250
+
+triangleLength = ( (4 / 3) * ( (letterHeight / 2) ** 2) ) ** (0.5)
 
 turtle.mode("logo")
 turtle.penup()
-turtle.goto(-10, -125)
+turtle.goto(0, 125)
 turtle.pendown()
 
 #START
-turtle.begin_fill()
-
-turtle.end_fill()
+turtle.setheading(150)
+turtle.forward(triangleLength)
+turtle.setheading(270)
+turtle.forward(triangleLength / 2)
+verticalStartingPoint = turtle.position()
+turtle.forward(triangleLength / 2)
+turtle.setheading(30)
+turtle.forward(triangleLength)
+turtle.penup()
+turtle.goto(verticalStartingPoint)
+turtle.pendown()
+turtle.setheading(180)
+turtle.forward(letterHeight / 4)
+turtle.setheading(90)
+turtle.forward(letterHeight / 4)
+turtle.setheading(270)
+turtle.forward(letterHeight / 2)
+turtle.setheading(90)
+turtle.forward(letterHeight / 4)
+turtle.setheading(180)
+turtle.forward(letterHeight / 4)
 #END
 
 turtle.done()
