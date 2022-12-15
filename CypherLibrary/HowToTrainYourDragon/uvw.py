69341071883141638fcf6cb417d659bce1172ae3
WaterbendingToph <dakotago@lakotaarms45@gmail.com>
2022-12-15 10:09:02 -0700

    Add Red Testing Lines HTTYD

--- CypherLibrary/HowToTrainYourDragon/uvw.py
+++ CypherLibrary/HowToTrainYourDragon/uvw.py
@@ -2,7 +2,7 @@ import turtle
 window = turtle.Screen()
 window.setup(width = 500, height=500)
 
-letterWidth = 200
+letterWidth = 250
 lineWidth = 20
 letterHeight = 250
 diagonal = 60
@@ -12,7 +12,30 @@ innerLine = (2 * ( (letterHeight - lineWidth - (lineWidth * 1.5) ) ** (2) ) ) **
 
 turtle.mode("logo")
 turtle.penup()
-turtle.goto(-10, -125)
+turtle.pencolor("red")
+turtle.goto(-250, -125)
+turtle.pendown()
+turtle.setheading(90)
+turtle.forward(500)
+turtle.penup()
+turtle.goto(-125, -250)
+turtle.pendown()
+turtle.setheading(0)
+turtle.forward(500)
+turtle.penup()
+turtle.goto(-250, 125)
+turtle.pendown()
+turtle.setheading(90)
+turtle.forward(500)
+turtle.penup()
+turtle.goto(125, 250)
+turtle.pendown()
+turtle.setheading(180)
+turtle.forward(500)
+turtle.penup()
+turtle.pencolor("blacK")
+
+turtle.goto(-105, -125)
 turtle.pendown()
 
 #START
