882decadb017f28826a16bb8c7ca88485eb7b059
WaterbendingToph <dakotago@lakotaarms45@gmail.com>
2023-01-05 11:39:17 -0700

    make FJ std vars account for direction

--- CypherLibrary/FluxJudonese/n.py
+++ CypherLibrary/FluxJudonese/n.py
@@ -3,10 +3,47 @@ window = turtle.Screen()
 window.setup(width = 360, height = 360)
 
 length =        40
-topLeft =       -140,140
-bottomLeft =    -140,-140
 ReturnPoint =   0,0
 direction = 1
+topLeft =       -140, 140
+if direction == 2:
+    topLeft =   140, 140
+if direction == 3:
+    topLeft =   -140, -140
+if direction == 4:
+    topLeft =   140, 140
+bottomLeft =    -140, -140
+if direction == 2:
+    bottomLeft = 140, -140
+if direction == 3:
+    bottomLeft = 140, -140
+if direction == 4:
+    bottomLeft = -140, 140
+
+up = 0
+if direction == 3:
+    up = 270
+if direction == 4:
+    up = 90
+right = 90
+if direction == 2:
+    right = 270
+if direction == 3:
+    right = 0
+if direction == 4:
+    right = 180
+down = 180
+if direction == 3:
+    down = 90
+if direction == 4:
+    down = 270
+left = 270
+if direction == 2:
+    left = 90
+if direction == 3:
+    left = 180
+if direction == 4:
+    left = 0
 
 turtle.speed(0)
 
