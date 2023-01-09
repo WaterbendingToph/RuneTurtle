4520f18473602a21842717358db2248f503ce6c2
WaterbendingToph <dakotago@lakotaarms45@gmail.com>
2023-01-09 16:53:25 -0700

    Setup Covenant File Structure

---
+++ CypherLibrary/Covenant/x.py
@@ -0,0 +1,43 @@
+import turtle
+window = turtle.Screen()
+window.setup(width = 500, height=500)
+
+letterHeight = 250
+
+turtle.mode("logo")
+turtle.penup()
+turtle.pencolor("red")
+turtle.goto(-250, 125)
+turtle.pendown()
+turtle.setheading(90)
+turtle.forward(500)
+
+turtle.penup()
+turtle.goto(125, 250)
+turtle.pendown()
+turtle.setheading(180)
+turtle.forward(500)
+
+turtle.penup()
+turtle.goto(250, -125)
+turtle.pendown()
+turtle.setheading(270)
+turtle.forward(500)
+
+turtle.penup()
+turtle.goto(-125, -250)
+turtle.pendown()
+turtle.setheading(0)
+turtle.forward(500)
+
+turtle.pencolor("black")
+turtle.penup()
+
+turtle.goto(-125, 125)
+turtle.pendown()
+
+#START
+
+#END
+
+turtle.done()
