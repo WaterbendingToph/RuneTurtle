ad401f4cfb1f5b3aa84b0c6f632602378414dca3
WaterbendingToph <dakotago@lakotaarms45@gmail.com>
2023-04-17 12:08:05 -0600

    Add Gnomish Symbol Files

---
+++ CypherLibrary/ArtemisFowl/d.py
@@ -0,0 +1,42 @@
+import turtle
+window = turtle.Screen()
+window.setup(width = 600, height=600)
+
+letterHeight = 400
+lineWidth = 20
+rightWall = 0
+returnPoint = 0,0
+
+turtle.mode("logo")
+turtle.penup()
+turtle.pencolor("red")
+turtle.goto(-300, -250)
+turtle.pendown()
+turtle.setheading(90)
+turtle.forward(600)
+turtle.penup()
+turtle.goto(250, -300)
+turtle.pendown()
+turtle.setheading(0)
+turtle.forward(600)
+turtle.penup()
+turtle.goto(300, 250)
+turtle.pendown()
+turtle.setheading(270)
+turtle.forward(600)
+turtle.penup()
+turtle.goto(-250, 300)
+turtle.pendown()
+turtle.setheading(180)
+turtle.forward(600)
+turtle.penup()
+turtle.pencolor("blacK")
+
+turtle.goto(-lineWidth / 2, -letterHeight / 2)
+turtle.pendown()
+
+
+#START
+#END
+
+turtle.done()
